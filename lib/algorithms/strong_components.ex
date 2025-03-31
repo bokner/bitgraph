58a4e9ea3276e0fc67835520297c8b2c98ee2f5b
Boris Okner <boris.okner@gmail.com>
2025-03-30 21:35:53 -0400

    Refactoring

--- lib/algorithms/strong_components.ex
+++ lib/algorithms/strong_components.ex
@@ -1,47 +1,5 @@
-defmodule BitGraph.Algorithms do
-  alias BitGraph.Dfs
-  alias BitGraph.{Array, Stack}
-  alias BitGraph.{E}
-
-  def topsort(graph) do
-    graph
-    |> Dfs.run()
-    |> then(fn state ->
-      Dfs.acyclic?(state) && Dfs.order(state, :out, :desc) || false
-    end)
-  end
-
-  def acyclic?(graph) do
-    graph
-    |> BitGraph.vertex_indices()
-    |> Enum.reduce_while(nil, fn v, state_acc ->
-      state_acc = Dfs.run(graph, v, state: state_acc)
-      Dfs.acyclic?(state_acc) && {:halt, true} || {:cont, state_acc}
-    end)
-    |> then(
-      fn true -> true
-      _ -> false
-    end)
-  end
-
-  def components(graph) do
-    Dfs.run(graph, :all, direction: :both, process_vertex_fun:
-      fn %{component_top: root, acc: acc} = _state, v  ->
-        case acc do
-          nil -> %{root => MapSet.new([root, v])}
-          existing ->
-            Map.update(existing, root, MapSet.new([root]),
-            fn component -> MapSet.put(component, v) end)
-          end
-      end
-      )
-      |> Map.get(:acc)
-      |> Map.values()
-  end
-
-  def strong_components(graph, component_handler \\ fn component, _state -> component end) do
-    kozaraju(graph, component_handler)
-  end
+defmodule BitGraph.Algorithms.SCC do
+  alias BitGraph.{Dfs, Array, Stack}
 
   ## Kozaraju's SCC algorithm
   def kozaraju(graph, component_handler \\ fn component, _state -> component end) do
@@ -139,46 +97,9 @@ defmodule BitGraph.Algorithms do
     end
   end
 
-  defp tarjan_pop_component(state, vertex) do
+  defp tarjan_pop_component(%{acc: %{stack: stack} = _acc} = state, vertex) do
     IO.inspect({"pop component", vertex}, label: :pop_component)
+    IO.inspect(Array.to_list(stack), label: :stack)
   end
-
-  def get_cycle(graph, vertex) when is_integer(vertex) do
-    if E.in_degree(graph, vertex) > 0 && E.out_degree(graph, vertex) > 0 do
-      Dfs.run(graph, vertex, process_edge_fun:
-        fn state, vertex, _neighbor, {:edge, :back}  ->
-              {:stop,
-                build_cycle(graph, vertex, state[:parent])
-              }
-           %{acc: acc} = _state, _vertex, _neighbor, _event ->
-              {:next, acc}
-        end
-      )
-      |> Map.get(:acc)
-    else
-      false
-    end
-  end
-
-    ## Build cycle starting from vertex
-    defp build_cycle(graph, start_vertex, parents_ref) do
-      build_cycle(graph, start_vertex, parents_ref, [start_vertex])
-    end
-
-    ## Move from vertices to their parents starting from given vertex
-    defp build_cycle(graph, start_vertex, parents_ref, acc) do
-      build_cycle(graph, start_vertex, Array.get(parents_ref, start_vertex), parents_ref, acc)
-    end
-
-    defp build_cycle(graph, start_vertex, vertex, parents_ref, acc) do
-      # Stop if we find vertex that is an out-neighbor of the vertex
-      # we started with.
-      if E.edge?(graph, start_vertex, vertex) do
-        [vertex | acc]
-      else
-        next_vertex = Array.get(parents_ref, vertex)
-        build_cycle(graph, start_vertex, next_vertex, parents_ref, [vertex | acc])
-      end
-    end
 
 end
--- lib/traversal/dfs.ex
+++ lib/traversal/dfs.ex
@@ -24,6 +24,7 @@ defmodule BitGraph.Dfs do
 
   def run(graph, vertices, opts) when is_list(vertices) do
     initial_state = Keyword.get(opts, :state) || init_dfs(graph, hd(vertices), opts)
+
     Enum.reduce(vertices, initial_state, fn vertex, state_acc ->
       if vertex_color(state_acc, vertex) == @white_vertex do
         ## New component discovered
@@ -36,13 +37,16 @@ defmodule BitGraph.Dfs do
 
   defp init_dfs(graph, root, opts) do
     num_vertices = BitGraph.num_vertices(graph)
+
     %{
       component_top: root,
       direction: Keyword.get(opts, :direction, :forward),
-      process_edge_fun: Keyword.get(opts, :process_edge_fun, default_process_edge_fun())
-      |> normalize_process_edge_fun(),
-      process_vertex_fun: Keyword.get(opts, :process_vertex_fun, default_process_vertex_fun())
-      |> normalize_process_vertex_fun(),
+      process_edge_fun:
+        Keyword.get(opts, :process_edge_fun, default_process_edge_fun())
+        |> normalize_process_edge_fun(),
+      process_vertex_fun:
+        Keyword.get(opts, :process_vertex_fun, default_process_vertex_fun())
+        |> normalize_process_vertex_fun(),
       ## Processing order for 'tree' edge:
       ## :preorder - before DFS call
       ## :postorder - after DFS call
@@ -66,20 +70,21 @@ defmodule BitGraph.Dfs do
   end
 
   defp dfs_impl(graph, vertex, %{direction: direction} = state)
-    when is_integer(vertex) do
-      time = inc_timer(state)
-      Array.put(state[:time_in], vertex, time)
-      Array.put(state[:color], vertex, @gray_vertex)
-      initial_state = process_vertex(state, vertex, :discovered)
-      Enum.reduce_while(vertex_neighbors(graph, vertex, direction), initial_state, fn
-        neighbor, state_acc ->
-          process_edge(graph, state_acc, vertex, neighbor)
-      end)
-      |> tap(fn _ ->
+       when is_integer(vertex) do
+    time = inc_timer(state)
+    Array.put(state[:time_in], vertex, time)
+    Array.put(state[:color], vertex, @gray_vertex)
+    initial_state = process_vertex(state, vertex, :discovered)
+
+    Enum.reduce_while(vertex_neighbors(graph, vertex, direction), initial_state, fn
+      neighbor, state_acc ->
+        process_edge(graph, state_acc, vertex, neighbor)
+    end)
+    |> tap(fn _ ->
       Array.put(state[:time_out], vertex, inc_timer(state))
       Array.put(state[:color], vertex, @black_vertex)
-      end)
-      |> process_vertex(vertex, :processed)
+    end)
+    |> process_vertex(vertex, :processed)
   end
 
   defp inc_timer(%{timer: timer} = _state) do
@@ -103,7 +108,8 @@ defmodule BitGraph.Dfs do
     end
   end
 
-  defp process_vertex(%{process_vertex_fun: process_vertex_fun} = state, vertex, event) when is_function(process_vertex_fun, 3) do
+  defp process_vertex(%{process_vertex_fun: process_vertex_fun} = state, vertex, event)
+       when is_function(process_vertex_fun, 3) do
     Map.put(state, :acc, process_vertex_fun.(state, vertex, event))
   end
 
@@ -111,23 +117,27 @@ defmodule BitGraph.Dfs do
     case vertex_color(state, neighbor) do
       @black_vertex ->
         ## (vertex, neighbor) is either a cross edge or forward edge
-        process_edge_impl(state, vertex, neighbor,
-          time_in(state, vertex) > time_in(state, neighbor) && :cross || :forward)
-      @gray_vertex  ->
+        process_edge_impl(
+          state,
+          vertex,
+          neighbor,
+          (time_in(state, vertex) > time_in(state, neighbor) && :cross) || :forward
+        )
+
+      @gray_vertex ->
         ## (vertex, neighbor) is a back edge
         process_edge_impl(Map.put(state, :dag, false), vertex, neighbor, :back)
+
       @white_vertex ->
         ## (vertex, neighbor) is a (dfs) tree edge)
         update_parent(state, neighbor, vertex)
         process_tree_edge(graph, state, vertex, neighbor, process_order)
     end
-
   end
 
   defp update_parent(%{parent: parent_ref} = _state, child, parent) do
     Array.put(parent_ref, child, parent)
   end
-
 
   ## Function for processing a vertex could be 2-arity (no options) or 3-arity
   defp normalize_process_vertex_fun(process_vertex_fun) when is_function(process_vertex_fun, 2) do
@@ -143,7 +153,9 @@ defmodule BitGraph.Dfs do
   ## Function for processing an edge could be 3-arity (ignoring edge type) or 4-arity
 
   defp normalize_process_edge_fun(process_edge_fun) when is_function(process_edge_fun, 3) do
-    fn state, prev_vertex, current_vertex, _edge_type -> process_edge_fun.(state, prev_vertex, current_vertex) end
+    fn state, prev_vertex, current_vertex, _edge_type ->
+      process_edge_fun.(state, prev_vertex, current_vertex)
+    end
   end
 
   defp normalize_process_edge_fun(process_edge_fun) when is_function(process_edge_fun, 4) do
@@ -152,19 +164,29 @@ defmodule BitGraph.Dfs do
 
   defp normalize_process_edge_fun(nil), do: nil
 
-  defp process_edge_impl(%{process_edge_fun: process_edge_fun} = state, start_vertex, end_vertex, edge_type) when is_function(process_edge_fun, 4) do
-    {next_action, acc} = case process_edge_fun.(state, start_vertex, end_vertex, {:edge, edge_type}) do
-      {:next, acc} -> {:next, acc}
-      {:stop, acc} -> {:stop, acc}
-      acc -> {:next, acc}
-    end
+  defp process_edge_impl(
+         %{process_edge_fun: process_edge_fun} = state,
+         start_vertex,
+         end_vertex,
+         edge_type
+       )
+       when is_function(process_edge_fun, 4) do
+    {next_action, acc} =
+      case process_edge_fun.(state, start_vertex, end_vertex, {:edge, edge_type}) do
+        {:next, acc} -> {:next, acc}
+        {:stop, acc} -> {:stop, acc}
+        acc -> {:next, acc}
+      end
+
     {to_reduce_while_result(next_action), Map.put(state, :acc, acc)}
   end
 
   defp process_tree_edge(graph, state, start_vertex, end_vertex, :preorder) do
     process_edge_impl(state, start_vertex, end_vertex, :tree)
-    |> then(fn {:halt, state} ->
+    |> then(fn
+      {:halt, state} ->
         {:halt, state}
+
       {:cont, state} ->
         {:cont, dfs_impl(graph, end_vertex, state)}
     end)
@@ -172,19 +194,20 @@ defmodule BitGraph.Dfs do
 
   defp process_tree_edge(graph, state, start_vertex, end_vertex, :both) do
     process_edge_impl(state, start_vertex, end_vertex, :tree)
-    |> then(fn {:halt, state} ->
-      {:halt, state}
-    {:cont, state} ->
-      dfs_impl(graph, end_vertex, state)
-      |> process_edge_impl(start_vertex, end_vertex, :tree)
+    |> then(fn
+      {:halt, state} ->
+        {:halt, state}
+
+      {:cont, state} ->
+        dfs_impl(graph, end_vertex, state)
+        |> process_edge_impl(start_vertex, end_vertex, :tree)
     end)
   end
 
-  defp process_tree_edge(graph,  state, start_vertex, end_vertex, :postorder) do
+  defp process_tree_edge(graph, state, start_vertex, end_vertex, :postorder) do
     dfs_impl(graph, end_vertex, state)
     |> process_edge_impl(start_vertex, end_vertex, :tree)
   end
-
 
   defp to_reduce_while_result(:next) do
     :cont
@@ -207,8 +230,10 @@ defmodule BitGraph.Dfs do
   end
 
   defp vertex_neighbors(graph, vertex, :both) do
-    MapSet.union(vertex_neighbors(graph, vertex, :forward),
-    vertex_neighbors(graph, vertex, :reverse))
+    MapSet.union(
+      vertex_neighbors(graph, vertex, :forward),
+      vertex_neighbors(graph, vertex, :reverse)
+    )
   end
 
   def acyclic?(state) do
@@ -254,5 +279,4 @@ defmodule BitGraph.Dfs do
   def time_out(%{time_out: ref} = _dfs_state, vertex) do
     Array.get(ref, vertex)
   end
-
 end
