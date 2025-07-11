e0bea72a0605ca16f430765a742812795baedf61
Boris Okner <boris.okner@gmail.com>
2025-07-10 21:35:39 -0400

    v0.1.19

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Bitgraph.MixProject do
   def project do
     [
       app: :bitgraph,
-      version: "0.1.18",
+      version: "0.1.19",
       elixir: "~> 1.17",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
